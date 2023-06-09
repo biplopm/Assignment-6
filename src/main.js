d8297785c9328586cbbf2c8bbd4563a3f226ece8
biplopm <kettyhomes6@gmail.com>
2023-06-10 02:26:30 +0600

    assingment

---
+++ src/main.js
@@ -0,0 +1,18 @@
+const search = prompt("Search Blood Member");
+
+const result = members.filter(
+  (data) =>
+    data.bloodGroup === search ||
+    data.name === search ||
+    data.age >= search ||
+    data.phoneNumber === search ||
+    data.donationHistory === search
+);
+
+const finalResult = [];
+
+result.forEach((item) => {
+  finalResult.push(item);
+});
+
+console.table(finalResult);
